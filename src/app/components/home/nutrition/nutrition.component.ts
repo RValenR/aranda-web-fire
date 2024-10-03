b274d6e22ecbeb5d3b0088acc7f4633b8954a66d
Alejo <rafael.valencia.r4@gmail.com>
2024-10-02 20:22:35 -0500

    boton agregar planta

--- src/app/components/home/nutrition/nutrition.component.ts
+++ src/app/components/home/nutrition/nutrition.component.ts
@@ -1,17 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-nutrition',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, TableModule],
   templateUrl: './nutrition.component.html',
   styleUrl: './nutrition.component.css'
 })
 export class NutritionComponent implements OnInit{
   firebaseService = inject(AuthService);
+  faPlus = faPlus;
+  public specialStyleClass = '';
+  items=[]
+  
+  constructor(private cdr: ChangeDetectorRef) {}
+
+
   ngOnInit(): void {
-    this.firebaseService.showInfo = true;
+    // this.firebaseService.showInfo = true;
+  }
+
+  addNewNutritionData(){
+
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.firebaseService.showInfo = true;
+      this.firebaseService.pageStyle = 'body-trimmed-aux'
+      // this.firebaseService.pageStyle = 'full-screen';
+      // Ahora forzamos la detección de cambios de forma segura
+      this.cdr.detectChanges();
+    }, 0);
   }
 
 }
